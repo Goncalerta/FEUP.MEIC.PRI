9407e6d4ea4ec7ac06e6e2fc515e053947b3217c
biromiro <n.r.t.c@hotmail.com>
2022-12-11 23:31:56 +0000

    Add query parser

--- backend/src/booksearch/solr_api.py
+++ backend/src/booksearch/solr_api.py
@@ -15,16 +15,30 @@ def make_query_raw(query):
 def make_query_basic(q="", fl="*, [child]", rows=10, start=0, sort="score desc", exact=False):
     # TODO this function probably should be improved to handle more complex queries
     #      and we could even create more highlevel wrappers as needed
+    fields = ["title", "subjects", "text"]
+    final_q = 'content_type: "BOOK"'
+    result = ""
 
-    result = str((TextBlob(q)).correct())
+    def boost(field):
+        if field == "title":
+            return 1
+        if field == "subjects":
+            return 1.5
+        if field == "text":
+            return 1
 
-    final_q = 'content_type: "BOOK"' + \
-        (" AND " + f'text:"{result}"' if result else "")
+    for keyword in q.split():
+        keyword_did_you_mean = keyword if exact else str(
+            (TextBlob(keyword)).correct())
+        result += keyword_did_you_mean + " "
+        final_q += " AND (" + (" OR ").join([
+            f'({field}:"{keyword_did_you_mean}"~)^{boost(field)}' for field in fields]) + ")"
 
+    print(final_q)
     return {
         "exact_query": exact,
         "orig_query": q,
-        "did_you_mean": result,
+        "did_you_mean": result.strip(),
         "docs": make_query_raw(f"q={final_q}&fl={fl}&rows={rows}&start={start}&sort={sort}")['response']['docs']
     }
 
@@ -34,7 +48,7 @@ def make_query_quote(q="", fl="*, [child]", rows=10, start=0, sort="score desc",
     result = str((TextBlob(q)).correct())
 
     final_q = 'content_type: "BOOK"' + \
-        (" AND " + f'text:"{result}"' if result else "")
+        (" AND " + f'text:"{result}"~' if result else "")
 
     return {
         "exact_query": exact,
