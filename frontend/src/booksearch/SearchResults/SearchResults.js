2200da9ecdce2e6901f2f9e76ce9186e1ddf0e7a
Pedro Gonçalo Correia <goncalerta@gmail.com>
2022-12-11 16:10:50 +0000

    Results

--- frontend/src/booksearch/SearchResults/SearchResults.js
+++ frontend/src/booksearch/SearchResults/SearchResults.js
@@ -1,16 +1,62 @@
+import { Box, Container, Rating, Typography } from "@mui/material";
 import PropTypes from "prop-types";
+import StarIcon from "@mui/icons-material/Star";
 
 function SearchResults(props) {
     const { loading, error, data } = props;
 
+    const ERROR_MESSAGE = "An error occurred :(";
+    const NO_RESULTS_MESSAGE = "No results found.";
+
+    const renderResult = (book) => (
+        <Container key={book.id} sx={{ marginTop: "3em" }}>
+            <Typography sx={{ fontSize: 34, margin: "0" }} color="text.secondary" gutterBottom>
+                <a href="/book/0">The adventures of this name has to change</a>
+            </Typography>
+            <Typography sx={{ fontSize: 18, margin: "0" }} color="text.secondary" gutterBottom>
+                Authored by Pedro Gonçalo (2001-), Nuno Costa (2001-) and William Deadspear
+                (1200-1210)
+            </Typography>
+            <Typography sx={{ fontSize: 12, margin: "0" }} color="text.secondary" gutterBottom>
+                Home Economics, Science, History, Geography, Art, Music, Drama, Physical, Erotic
+            </Typography>
+            <Typography sx={{ fontSize: 12, margin: "0" }} color="text.secondary" gutterBottom>
+                Released on 17/04/2023
+            </Typography>
+            <Box
+                sx={{
+                    width: 200,
+                    display: "flex",
+                    alignItems: "center",
+                }}
+            >
+                <Rating
+                    value={4}
+                    readOnly
+                    precision={0.5}
+                    emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+                />
+                <Typography
+                    sx={{ fontSize: 14, margin: "0", marginLeft: "0.2em", lineHeight: "0.1em" }}
+                    color="text.secondary"
+                    gutterBottom
+                >
+                    • 13 ratings
+                </Typography>
+            </Box>
+        </Container>
+    );
+
     return (
-        <div>
-            RESULTS <br />
+        <Container>
+            {/* <Typography sx={{ fontSize: 34 }} color="text.secondary" gutterBottom>
+                Results
+            </Typography> */}
             {loading && <p>Loading...</p>}
-            {error && <p>Error</p>}
-            {!loading && !error && data.length === 0 && <p>No results</p>}
-            {!loading && !error && data.map((book) => <div key={book.id}>{book.title}</div>)}
-        </div>
+            {error && <p>{ERROR_MESSAGE}</p>}
+            {!loading && !error && data.length === 0 && <p>{NO_RESULTS_MESSAGE}</p>}
+            {!loading && !error && data.map((book) => renderResult(book))}
+        </Container>
     );
 }
 
